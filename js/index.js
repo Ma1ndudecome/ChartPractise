7c1baaf415c1a22a83dd988f808c1020a7a41051
Ma1n <kskiller2020@gmail.com>
2025-01-17 12:44:34 +0200

    add new users, change color chart

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const ctx = document.getElementById('myChart');
-Chart.defaults.backgroundColor = '#9BD0F5';
-Chart.defaults.borderColor = '#36A2EB';
+Chart.defaults.backgroundColor = 'lightgreen';
+Chart.defaults.borderColor = 'pink';
 Chart.defaults.color = 'red';
 const data ={
-    labels:['Andrei', 'Vlad', 'Main'],
+    labels:['Andrei', 'Vlad', 'Main', 'Some', 'Jeka', 'Illya', 'Nadya'],
     datasets:[{
         label:'Progressive',
-        data:[10, 9, 12],
+        data:[25,26, 30, 15, 12, 6, 17 ],
         borderWidth:1
     }]
 
@@ -20,14 +20,14 @@ const config = {
           duration: 1000,
           easing: 'linear',
           from: 0,
-          to: 1,
+          to: 0.5,
           loop: true
         }
       },
       scales: {
         y: { 
           min: 0,
-          max: 20
+          max: 40
         }
       }
     }
